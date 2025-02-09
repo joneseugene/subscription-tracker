d0d37d1338f1671dc0ac32275d90aef28e67914c
joneseugene <joneseugeneceo@gmail.com>
2025-02-09 11:13:09 +0000

    Implemented Bearer token authorization

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { getUser, getUsers } from '../controllers/user.controller.js';
+import authorize from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
 userRouter.get('/', getUsers);
-userRouter.get('/:id', getUser);
+userRouter.get('/:id', authorize, getUser);
 userRouter.post('/', (req, res) => res.send( {title: 'create user'}));
 userRouter.put('/:id', (req, res) => res.send( {title: 'update user'}));
 userRouter.delete('/:id', (req, res) => res.send( {title: 'delete user'}));
