7b09d21b020be4330e948a13e8e192dc7638da7f
joneseugene <joneseugeneceo@gmail.com>
2025-02-09 06:41:22 +0000

    Connected to Mongo DB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from './config/env.js'
 import authRouter from './routes/auth.routes.js';
 import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDb from './database/mongodb.js';
 
 const app = express();
 
@@ -15,8 +16,9 @@ app.get('/', (req, res) => {
     res.send('Welcome to my Subscription Tracker API' )
 });
 
-app.listen(PORT, ()=> {
+app.listen(PORT, async () => {
     console.log(`This API on localhost http://localhost:${PORT}`)
+    await connectToDb()
 })
 
 export default app
